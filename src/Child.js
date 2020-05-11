0cc8fa70e3ba88733fadcd0295e34b5bf756dde9
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-11 17:36:32 -0400

    Done.

--- src/Child.js
+++ src/Child.js
@@ -3,10 +3,18 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    // will log to console handleColorChange
+    // which is equal to this.changeColor
+    console.log(this.props)
+    //add React event handler - onClick
+    //change Child component background color based on Child state
     return (
-      <div
+      <div /* onClick={this.props.handleColorChange} */
+           /* made to be a function since Parent component changeColor method takes in an argument */
+        onClick={() => this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        // style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
--- src/Parent.js
+++ src/Parent.js
@@ -6,16 +6,33 @@ class Parent extends Component {
 
   constructor() {
     super()
+    //initial state
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      //store the state of the Child component
+      childrenColor: '#FFF'
     }
+  }
+
+  // instance method to change state
+  // will be passed to child components as a prop
+  // child components will be able to invoke and change parent's state
+  // will also change the color state of the Child components
+  // added parameter to allow Child components to send data back to Parent
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor // added to represent new changed state
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        {/* add callback function as a prop that changes color on click */}
+        {/* pass down Child component state data as a prop */}
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
